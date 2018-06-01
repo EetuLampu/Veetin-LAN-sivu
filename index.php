8eb596a3f8a3436ef78b12209cc958f554140fb0
EetuLampu <35563468+EetuLampu@users.noreply.github.com>
2018-06-01 10:19:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fi">
+<?php
+require "./yhteys.php";
+
+if(!empty($_POST["nimi"]) && !empty($_POST["email"]) && !empty($_POST["paikka"]))
+{
+	$nimi=$_POST["nimi"];
+	$email=$_POST["email"];
+	$paikka=$_POST["paikka"];
+	
+	
+	
+	$sql="INSERT INTO lan(nimi,email,PaikkaID) VALUES('$nimi','$email','$paikka')";
+	
+	$lause=$yhteys->prepare($sql);
+	$lause->execute();
+	
+	header("Location:./onnittelut.php");
+}
+?>
+<head>
+	<meta charset="utf-8">
+	<title>Lan-ilmoitautuminen</title>
+	<link rel="stylesheet" type="text/css" href="./css.css">
+	<link rel="icon" href="./Kuvat/logo.png">
+</head>
+<body>
+
+	<div class="header">
+		<a class="brand" href="./index.php"  style="color: rgb(41, 163, 41); text-decoration: none"><img src="./kuvat/logo.png" style="width: 15%"></a>
+	</div>
+<div class="container">
+  <form action="./index.php" method="post">
+	<h2>Anna tietosi</h2>
+	<p><label for="nimi">nimi *</label><br />
+	<input type="text" name="nimi" required><br /></p>
+	<p><label for="email">Sähköposti *</label><br />
+	<input type="email" name="email" required><br /></p>
+	<p><label for="paikka">paikka *</label><br />
+		<?php
+					require_once("./yhteys.php");
+					
+					$sql = "SELECT PaikkaID FROM paikka";
+					$result = mysqli_query($connection, $sql);
+					
+					echo "<select name='paikka' required>";
+					while ($row = mysqli_fetch_array($result)) {
+						echo "<option value='" . $row['PaikkaID'] . "'>" . $row['PaikkaID'] . "</option>";
+					}
+					echo "</select>";
+					?>
+	<p><input class="button" type="submit" value="Varaa paikka"></p>
+	</form>
+</div>
+	<footer class="footer">
+        <p>LAN</p>
+    </footer>
---
+++ onnittelut.php
@@ -0,0 +1,2 @@
+<p>Onnistui</p>
+<meta http-equiv="refresh" content="3;url=http://magnesium/16tvpta/eetu.lampu/veeti/">
